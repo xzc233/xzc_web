7023b87bb9319a91db14e35f0795c8f170a9f91b
xzc233 <74583378+xzc233@users.noreply.github.com>
2020-11-30 17:54:20 +0800

    Update GoodDao.java

--- src/main/java/cn/xzc/dao/GoodDao.java
+++ src/main/java/cn/xzc/dao/GoodDao.java
@@ -10,23 +10,17 @@ public class GoodDao {
 	public Good find(String id) {
 		Good good=new Good();
 		try{
-			Class.forName("com.mysql.jdbc.Driver"); 
-			// 数据库连接字符串 
+			Class.forName("com.mysql.jdbc.Driver");  
 			String url = "jdbc:mysql://localhost:3306/good?useUnicode=true&characterEncoding=utf8&useSSL=false"; 
-			// 数据库用户名 
 			String username = "root"; 
-			// 数据库密码 
 			String password = "991213"; 
-			// 创建Connection连接 
 			Connection conn = DriverManager.getConnection(url, username, password); 
-	    
-			Statement state=conn.createStatement();//容器
-			String sql="select * from good where id="+id;           //sql语句
-			
-			ResultSet rs=state.executeQuery(sql);     //将sql语句传至数据库，返回的值为一个字符集用一个变量接收 
+			Statement state=conn.createStatement();
+			String sql="select * from good where id="+id;           
+			ResultSet rs=state.executeQuery(sql);    
 			while(rs.next()){
-				good.setId(Integer.parseInt(rs.getString(1)));	 //getString（n）获取第n列的内容
-				good.setName(rs.getString(2)); 						//数据库中的列数是从1开始的
+				good.setId(Integer.parseInt(rs.getString(1)));	 
+				good.setName(rs.getString(2)); 						
 				good.setPrice(Double.parseDouble(rs.getString(3)));
 				good.setDescription(rs.getString(4));
 			}
