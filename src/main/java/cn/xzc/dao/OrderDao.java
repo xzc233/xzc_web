bea2829ee659bf9e42c27a1e349662d8806b01a7
xzc233 <74583378+xzc233@users.noreply.github.com>
2020-11-30 17:55:16 +0800

    Update OrderDao.java

--- src/main/java/cn/xzc/dao/OrderDao.java
+++ src/main/java/cn/xzc/dao/OrderDao.java
@@ -6,21 +6,6 @@ import cn.xzc.domain.Order;
 	public interface OrderDao {
 
 		void add(Order o);
-
-		/*
-		 * 最麻烦的一个方法
-		 * 这个方法要把Order对象的基本信息找回来，还要把用户的信息找回来，还要把多个订单项的信息找回来。
-		 * 那么就要查找4张表。
-		 * 
-		 */
-		Order find(String id);
-
-		/*
-		 * 查看已经发货或没发货的订单信息
-		 * 
-		 * status为true，表示已发货
-		 * status为false，表示未发货
-		 */
 		List<Order> getAll(boolean status);
 		
 	}
